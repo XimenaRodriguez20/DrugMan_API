1573e69119117b123c1a913e71b4ef86c0ee1b52
XimenaRodriguez20 <123812926+ximenarodriguez20@users.noreply.github.com>
2023-11-27 18:05:31 -0500

    pruebas de cobertura

---
+++ src/test/java/com/game/drugman/DrugMan_APITest.java
@@ -0,0 +1,22 @@
+package com.game.drugman;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+public class DrugMan_APITest {
+    @Test
+    void contextLoads() {
+        boolean prueba = true;
+        assertEquals(true, prueba);
+    }
+
+    @Test
+    void context() {
+        boolean prueba = false;
+        assertEquals(false, prueba);
+    }
+
+}
